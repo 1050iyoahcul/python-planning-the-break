67a9a3adf6ba7dc2dd72d5658f4e901c4f39f8fe
luchaoyi <iyoahcul@gmail.com>
2017-08-09 19:28:53 +0800

    refactor: add plan of morning

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # python-planning-the-break
 
-
-- [ ] 开机打开ide
-- [ ] 开机打开chrome
-- [ ] 开机打开terminal
-
+- [ ] 09:30上网
+- [x] 10:30上网打开ide chrome terminal
+- [ ] 11:30提醒抽烟
+- [ ] 11:40上网
+- [ ] 12:00自动锁屏
+- [ ] 13:00提醒工作
--- init.py
+++ init.py
@@ -1,5 +1,40 @@
 import subprocess
+import webbrowser
+import os
+from Xlib import X, display, error, Xatom, Xutil
+import Xlib.protocol.event
 
-subprocess.call(["ls", "-a"])
+
+def sendEvent(win, ctype, data, mask=None):
+    data = (data + [0] * (5 - len(data)))[:5]
+    ev = Xlib.protocol.event.ClientMessage(window=win, client_type=ctype, data=(32, (data)))
+    if not mask:
+        mask = (X.SubstructureRedirectMask | X.SubstructureNotifyMask)
+    root.send_event(ev, event_mask=mask)
 
 
+# connect shadowsocks
+
+display = Xlib.display.Display()
+screen = display.screen()
+root = screen.root
+
+sendEvent(root, display.intern_atom("_NET_CURRENT_DESKTOP"), [0, X.CurrentTime])
+display.flush()
+webbrowser.open('https://duckduckgo.com/')
+
+# switch to desktop 2
+# open ide
+sendEvent(root, display.intern_atom("_NET_CURRENT_DESKTOP"), [1, X.CurrentTime])
+display.flush()
+subprocess.call(["bash", "/opt/pycharm/bin/pycharm.sh"])
+# subprocess.call(["bash", "/opt/phpstorm/bin/phpstorm.sh"])
+
+# switch to desktop 3
+# open terminal
+sendEvent(root, display.intern_atom("_NET_CURRENT_DESKTOP"), [2, X.CurrentTime])
+display.flush()
+os.system("gnome-terminal -e 'bash -c \"cd ~/Code; exec bash\"'")
+
+# switch to desktop 3
+# open mysql-workbench
